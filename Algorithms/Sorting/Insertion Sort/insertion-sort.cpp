53be89d36ffacca5887d5fb50d3ff94af55692df
Agrim Mittal <agrimmittal97@gmail.com>
2018-04-15 23:45:58 +0530

    update insertion sort

--- Algorithms/Sorting/Insertion Sort/insertion-sort.cpp
+++ Algorithms/Sorting/Insertion Sort/insertion-sort.cpp
@@ -10,16 +10,20 @@ using namespace std;
 void printArray(int arr[], int size);
 
 int insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i-1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
-            j--;
+    for (int j = 1; j < n; ++j) {
+        int key = arr[j];
+        int i = j-1;
+        while (i >= 0 && arr[i] > key) {
+
+            arr[i+1] = arr[i];
+            cout<<"In while loop with i = "<<i<<endl;
+            printArray(arr, n);
+            i--;
         }
-        arr[j+1] = key;
+        arr[i+1] = key;
+        cout<<"In for loop with j = "<<j<<endl;
+        printArray(arr, n);
     }
-    printArray(arr, n);
 }
 
 void printArray(int arr[], int size) {
@@ -29,9 +33,10 @@ void printArray(int arr[], int size) {
 }
 
 int main(int argc, char const *argv[]) {
-    int arr[] = {3,1,5,2,75,7,4,12,35,54,1,256,24,5736453,1,7};
+    int arr[] = {5,2,4,6,1,3};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout<<"Insertion Sort => ";
+    cout<<"Insertion Sort => "<<endl;
+    printArray(arr, n);
     insertionSort(arr, n);
     return 0;
 }
